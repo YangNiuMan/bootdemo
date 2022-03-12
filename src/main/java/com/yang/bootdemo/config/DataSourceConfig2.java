6cf6446495a70d85136b13c6670ebf7d4a1cffc6
yangjie <18226032343@163.com>
2022-03-12 23:11:05 +0800

    springboot整合多数据源

---
+++ src/main/java/com/yang/bootdemo/config/DataSourceConfig2.java
@@ -0,0 +1,49 @@
+package com.yang.bootdemo.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * @author yang
+ */
+@Configuration
+@MapperScan(basePackages="com.yang.bootdemo.mapper.db2", sqlSessionFactoryRef = "db2SqlSessionFactory")
+public class DataSourceConfig2 {
+
+
+    @Bean("db2DataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.db2") //读取application.yml中的配置参数映射成为一个对象
+    public DataSource getDb2DataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean("db2SqlSessionFactory")
+    public SqlSessionFactory db2SqlSessionFactory(@Qualifier("db2DataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        // mapper的xml形式文件位置必须要配置，不然将报错：no statement （这种错误也可能是mapper的xml中，namespace与项目的路径不一致导致）
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapping/db2/*.xml"));
+        return bean.getObject();
+    }
+
+    @Primary
+    @Bean("db2SqlSessionTemplate")
+    public SqlSessionTemplate db2SqlSessionTemplate(@Qualifier("db2SqlSessionFactory") SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+
+}
