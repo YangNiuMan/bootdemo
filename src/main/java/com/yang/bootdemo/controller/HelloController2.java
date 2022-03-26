44e17ef3dc1affa8e882ee4cc60fa518e4b9e59d
yangjie <18226032343@163.com>
2022-03-26 23:20:30 +0800

    cotroller2

---
+++ src/main/java/com/yang/bootdemo/controller/HelloController2.java
@@ -0,0 +1,25 @@
+package com.yang.bootdemo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yang
+ */
+@RestController
+public class HelloController2 {
+
+    @RequestMapping("hello")
+    public String hello(String message){
+        System.out.println(message);
+        System.out.println(message+"222");
+        System.out.println(message+"333");
+        System.out.println(message+"555");
+        System.out.println(message+"666");
+        System.out.println(message+"777");
+        return message;
+    }
+
+
+}
